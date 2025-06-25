b5c4870017ece1d356d8ec5aa505d5ce6bcf61ef
bhupendra gariya <bhupiigarya7@gmail.com>
2025-06-25 10:54:09 +0530

    use route

--- backend/routes/userRouts.js
+++ backend/routes/userRouts.js
@@ -10,6 +10,9 @@ router.post("/verify-otp", verifyOTP);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
 router.put("/change-password", isUserAuthenticated, changePasswordUser);
+router.post("/forgotPassword" , isUserAuthenticated, forgotPassword)
+router.put("/resetPassword" , isUserAuthenticated, resetPassword)
+
 
 
 export default router;
